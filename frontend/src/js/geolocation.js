c05e14416fbe369dd242da73b2993b092edf1923
liu-01987175 <132244772+liu-01987175@users.noreply.github.com>
2025-03-25 21:21:44 -0400

    Add files via upload
    working example for Geolocation API (don't merge with main)
    - when running Chrome and pressing the "Get location button", the user will be prompted to "Allow when visiting, Allow this time, or Decline"
    - when accepting the geolocation prompt, data will be in this format: "Latitude: 42.6523957, Longitude: -71.3237856"

---
+++ frontend/src/js/geolocation.js
@@ -0,0 +1,30 @@
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        document.getElementById("location").innerText = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    document.getElementById("location").innerText = `Latitude: ${lat}, Longitude: ${lon}`;
+}
+
+function showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            document.getElementById("location").innerText = "User denied the request for Geolocation.";
+            break;
+        case error.POSITION_UNAVAILABLE:
+            document.getElementById("location").innerText = "Location information is unavailable.";
+            break;
+        case error.TIMEOUT:
+            document.getElementById("location").innerText = "The request to get user location timed out.";
+            break;
+        case error.UNKNOWN_ERROR:
+            document.getElementById("location").innerText = "An unknown error occurred.";
+            break;
+    }
+}
