92ef0d96199014a745095e35adebf795bd8e5c1d
GrahamLaroche <glaroche2001@gmail.com>
2025-03-27 22:37:58 -0400

    Main Page frontend work

---
+++ main.js
@@ -0,0 +1,9 @@
+/*
+Main behavior is getting the address finder working
+stuff like signup and other pages can come later
+
+also something important is that we absolutely want to limit our api calls as much as possible
+unlikely we will ever reach 10k requests but its essential that we dont bypass it for any reason
+
+most of this stuff is more of a backend problem though
+*/
